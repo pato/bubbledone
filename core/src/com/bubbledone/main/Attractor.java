6c0c0ea12be1555cefbf7d33e67a6210022b19d1
Pato <plankenau@gmail.com>
2014-10-18 17:54:03 -0500

    removed graviational constant

--- core/src/com/bubbledone/main/Attractor.java
+++ core/src/com/bubbledone/main/Attractor.java
@@ -3,13 +3,12 @@ package com.bubbledone.main;
 import com.badlogic.gdx.math.Vector2;
 
 public class Attractor {
-	private float mass, radius, gravitational_constant;
+	private float mass, radius;
 	private Vector2 position;
 	
-	public Attractor(float mass, float radius, float gravitational_constant, float x, float y) {
+	public Attractor(float mass, float radius, float x, float y) {
 		this.mass = mass;
 		this.radius = radius;
-		this.gravitational_constant = gravitational_constant;
 		this.position = new Vector2(x, y);
 	}
 }
