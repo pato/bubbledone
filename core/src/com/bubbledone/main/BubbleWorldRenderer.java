608a90474f218bf471720a0743413f606c6edd17
Rohan Ramchand <rohan@cs.utexas.edu>
2014-10-18 17:40:45 -0500

    indenting fixes

--- core/src/com/bubbledone/main/BubbleWorldRenderer.java
+++ core/src/com/bubbledone/main/BubbleWorldRenderer.java
@@ -9,31 +9,31 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 public class BubbleWorldRenderer {
 	private BubbleWorld world;
 	private OrthographicCamera cam;
-    private ShapeRenderer shapeRenderer;
-	
+	private ShapeRenderer shapeRenderer;
+
 	public BubbleWorldRenderer(BubbleWorld world) {
 		this.world = world;
-		
+
 		cam = new OrthographicCamera();
-        cam.setToOrtho(true, 136, 204);
-        shapeRenderer = new ShapeRenderer();
-        shapeRenderer.setProjectionMatrix(cam.combined);
+		cam.setToOrtho(true, 136, 204);
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setProjectionMatrix(cam.combined);
 	}
+	
 	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+		shapeRenderer.rect(world.getRect().x, world.getRect().y,
+				world.getRect().width, world.getRect().height);
+		shapeRenderer.end();
 
-        shapeRenderer.begin(ShapeType.Filled);
-        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-        shapeRenderer.rect(world.getRect().x, world.getRect().y,
-        		world.getRect().width, world.getRect().height);
-        shapeRenderer.end();
-
-        shapeRenderer.begin(ShapeType.Line);
-        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-        shapeRenderer.rect(world.getRect().x, world.getRect().y,
-        		world.getRect().width, world.getRect().height);
-        shapeRenderer.end();
-    }
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+		shapeRenderer.rect(world.getRect().x, world.getRect().y,
+				world.getRect().width, world.getRect().height);
+		shapeRenderer.end();
+	}
 }
