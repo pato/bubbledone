865b20c6c6ceeddf5b563dcec23a7f26ee7783dc
Rohan Ramchand <rohan@cs.utexas.edu>
2014-10-18 17:01:41 -0500

    added some basic rendering stuff

--- core/src/com/bubbledone/main/TaskBubble.java
+++ core/src/com/bubbledone/main/TaskBubble.java
@@ -1,6 +1,6 @@
 package com.bubbledone.main;
 
-import java.util.Calendar;
+import com.badlogic.gdx.math.Vector2;
 
 /*
  * Bubble class for task
@@ -8,16 +8,21 @@ import java.util.Calendar;
 
 public class TaskBubble {
 	private Task task;
-	
-	public TaskBubble(Task task) {
+	private int width, height;
+	private Vector2 position, velocity, acceleration;
+
+	public TaskBubble(Task task, int width, int height, float x, float y) {
 		this.task = task;
+		this.width = width;
+        this.height = height;
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        acceleration = new Vector2(0, 460);
 	}
 	
-	public TaskBubble(String name, Calendar dueDate) {
-		this.task = new Task(name, dueDate);
-	}
-	
-	public void physics() {
-		
+	public void update(float x, float y, float x_vel, float y_vel, float x_acc, float y_acc) {
+		position.set(x, y);
+		velocity.set(x_vel, y_vel);
+		acceleration.set(x_acc, y_acc);
 	}
 }
