b070e2c212f2b5ef751f4bfa0f8563835f65ba31
Rohan Ramchand <rohan@cs.utexas.edu>
2014-10-18 17:33:52 -0500

    added TaskBubbleRenderer and update method

--- core/src/com/bubbledone/main/TaskBubble.java
+++ core/src/com/bubbledone/main/TaskBubble.java
@@ -1,5 +1,6 @@
 package com.bubbledone.main;
 
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 /*
@@ -9,20 +10,81 @@ import com.badlogic.gdx.math.Vector2;
 public class TaskBubble {
 	private Task task;
 	private int width, height;
-	private Vector2 position, velocity, acceleration;
+	private Vector2 position, velocity;
+	private float radius;
+	
+	private Circle circle;
 
-	public TaskBubble(Task task, int width, int height, float x, float y) {
+	public TaskBubble(Task task, int width, int height, float x, float y, float radius) {
 		this.task = task;
 		this.width = width;
         this.height = height;
         position = new Vector2(x, y);
         velocity = new Vector2(0, 0);
-        acceleration = new Vector2(0, 460);
+        this.radius = radius;
+        
+        circle = new Circle(position, radius);
 	}
 	
-	public void update(float x, float y, float x_vel, float y_vel, float x_acc, float y_acc) {
+	public Circle getCircle() {
+		return circle;
+	}
+	
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	
+	public Task getTask() {
+		return task;
+	}
+
+	public void setTask(Task task) {
+		this.task = task;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
+	}
+
+	public float getRadius() {
+		return radius;
+	}
+
+	public void setRadius(float radius) {
+		this.radius = radius;
+	}
+
+	public void update(float x, float y, float x_vel, float y_vel, float radius) {
 		position.set(x, y);
 		velocity.set(x_vel, y_vel);
-		acceleration.set(x_acc, y_acc);
+		this.radius = radius;
 	}
 }
--- core/src/com/bubbledone/main/TaskBubbleRenderer.java
+++ core/src/com/bubbledone/main/TaskBubbleRenderer.java
@@ -1,10 +1,30 @@
 package com.bubbledone.main;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
 public class TaskBubbleRenderer implements Renderer {
 	TaskBubble bubble;
+	private OrthographicCamera cam;
+    private ShapeRenderer shapeRenderer;
+    private BubbleWorld world;
+	
+	public TaskBubbleRenderer(TaskBubble bubble, OrthographicCamera cam,
+			ShapeRenderer shapeRenderer, BubbleWorld world) {
+		this.bubble = bubble;
+		this.cam = cam;
+		this.shapeRenderer = shapeRenderer;
+		this.world = world;
+	}
 	
 	@Override
 	public void render() {
-		
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(87 / 255.0f, 87 / 255.0f, 87 / 255.0f, 1);
+        shapeRenderer.circle(bubble.getCircle().x, bubble.getCircle().y, bubble.getCircle().radius);
+        shapeRenderer.end();
 	}
 }
