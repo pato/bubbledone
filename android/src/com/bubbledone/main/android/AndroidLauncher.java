a012c78386c15d1ce30dc0262435c08a20551735
Pato <plankenau@gmail.com>
2014-10-18 11:40:11 -0500

    initial commit

---
+++ android/src/com/bubbledone/main/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bubbledone.main.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bubbledone.main.BubbleDone;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BubbleDone(), config);
+	}
+}
---
+++ desktop/src/com/bubbledone/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bubbledone.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bubbledone.main.BubbleDone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BubbleDone(), config);
+	}
+}
