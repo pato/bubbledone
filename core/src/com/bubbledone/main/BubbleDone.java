c6973958776151faf38e819f6a33f76d3927c045
Pato <plankenau@gmail.com>
2014-10-18 15:59:04 -0500

    added bubble screen, cleared our core project

--- core/src/com/bubbledone/main/BubbleDone.java
+++ core/src/com/bubbledone/main/BubbleDone.java
@@ -1,54 +1,13 @@
 package com.bubbledone.main;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.Game;
+import com.bubbledone.screens.BubbleScreen;
 
-public class BubbleDone extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	OrthographicCamera camera;
-	World world;
-	Box2DDebugRenderer debugRenderer;
-	
+public class BubbleDone extends Game {
+
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new BubbleScreen());
 		
-		camera = new OrthographicCamera();
-		
-		world = new World(new Vector2(0, -10), true);
-		debugRenderer = new Box2DDebugRenderer();
-	}
-
-	private float accumulator = 0;
-
-	private void doPhysicsStep(float deltaTime) {
-	    // fixed time step
-	    // max frame time to avoid spiral of death (on slow devices)
-	    float frameTime = Math.min(deltaTime, 0.25f);
-	    accumulator += frameTime;
-	    while (accumulator >= Constants.TIME_STEP) {
-	        world.step(Constants.TIME_STEP, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
-	        accumulator -= Constants.TIME_STEP;
-	    }
-	}
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-		
-		debugRenderer.render(world, camera.combined);
-		doPhysicsStep(0.5f);
 	}
 }
