3b8ec080d763f0189e8cd65f15187263d22fded0
Pato <plankenau@gmail.com>
2014-10-18 17:47:00 -0500

    added bubbles

--- core/src/com/bubbledone/main/BubbleWorldRenderer.java
+++ core/src/com/bubbledone/main/BubbleWorldRenderer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import java.util.List;
 
 public class BubbleWorldRenderer {
 	private BubbleWorld world;
@@ -23,17 +24,23 @@ public class BubbleWorldRenderer {
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        List<TaskBubble> bubbles = world.getBubbles();
+        
+        for (TaskBubble b : bubbles) {
+        	
+        }
 
-        shapeRenderer.begin(ShapeType.Filled);
-        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-        shapeRenderer.rect(world.getRect().x, world.getRect().y,
-        		world.getRect().width, world.getRect().height);
-        shapeRenderer.end();
-
-        shapeRenderer.begin(ShapeType.Line);
-        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-        shapeRenderer.rect(world.getRect().x, world.getRect().y,
-        		world.getRect().width, world.getRect().height);
-        shapeRenderer.end();
+//        shapeRenderer.begin(ShapeType.Filled);
+//        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+//        shapeRenderer.rect(world.getRect().x, world.getRect().y,
+//        		world.getRect().width, world.getRect().height);
+//        shapeRenderer.end();
+//
+//        shapeRenderer.begin(ShapeType.Line);
+//        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+//        shapeRenderer.rect(world.getRect().x, world.getRect().y,
+//        		world.getRect().width, world.getRect().height);
+//        shapeRenderer.end();
     }
 }
