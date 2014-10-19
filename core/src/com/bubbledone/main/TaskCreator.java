eff2ddfee5304198c18b0b086a4f1bb6927dceb4
Tres Popp <theodore.popp3@gmail.com>
2014-10-18 21:39:28 -0500

    random callback junk

--- core/src/com/bubbledone/main/TaskCreator.java
+++ core/src/com/bubbledone/main/TaskCreator.java
@@ -10,21 +10,22 @@ import java.util.Locale;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class TaskCreator implements Screen {
+
+	int timeToComplete, dueTime;
+	Calendar dueDate;
+	String task;
 
 	private String[] bubbleProperties = new String[4];
 	private int i = 0;
 	private String[] prompts = { "Task", "Time to Completion (minutes)",
 			"Due date (MM/DD/YY)", "Due time (HH:MM)" };
 	protected BubbleDone parent;
-	SimpleDateFormat sdfNums = new SimpleDateFormat("MM/dd/yyyy HH:mm", Locale.US);
-	SimpleDateFormat sdfLetters = new SimpleDateFormat("MMM dd, yyyy HH:mm", Locale.US);
+	SimpleDateFormat sdfNums = new SimpleDateFormat("MM/dd/yyyy HH:mm",
+			Locale.US);
+	SimpleDateFormat sdfLetters = new SimpleDateFormat("MMM dd, yyyy HH:mm",
+			Locale.US);
 
 	public TaskCreator(BubbleDone parent) {
 		this.parent = parent;
@@ -32,53 +33,69 @@ public class TaskCreator implements Screen {
 		Gdx.input.getTextInput(new TextInput(TaskCreator.this), "New Task",
 				prompts[i]);
 	}
+	
+	public void next(Calendar cal){
+		this.dueDate = cal;
+		Gdx.input.getTextInput(new TextInput(TaskCreator.this), prompts[i++],
+				"12:00");
+	}
+	
+	public void next(long time){
+	}
 
 	public void next(String text) {
-		if (i == 1) {
+		switch (i) {
+		case 0:
+			
+			break;
+		case 1:
 			if (!text.trim().matches("\\d+")) {
 				Gdx.input.getTextInput(new TextInput(TaskCreator.this),
 						"New Task", prompts[i]);
+			} else {
+				
 			}
-		} else if (i == 2) {
+			break;
+		case 2:
 			if (text.trim().matches("\\d{1,2}/\\d{1,2}/\\d{2,4}")) {
-				
-			} else if(text.trim().matches("\\w{3,4}\\s+\\d{1,2},\\s+\\d{2,4}")){
-				
+
+			} else if (text.trim().matches("\\w{3,4}\\s+\\d{1,2},\\s+\\d{2,4}")) {
+
 			} else {
 				Gdx.input.getTextInput(new TextInput(TaskCreator.this),
 						"New Task", prompts[i]);
 			}
-		} else if (i == 3) {
+			break;
+		case 3:
 			if (!text.trim().matches("\\d{1,2}:\\d{2}")) {
 				Gdx.input.getTextInput(new TextInput(TaskCreator.this),
 						"New Task", prompts[i]);
 			}
+			break;
+//		if (i >= 4) {
+//			Calendar cal = Calendar.getInstance();
+//			System.err.println("making task");
+//			try {
+//				if (bubbleProperties[2].trim().matches(
+//						"\\d{1,2}/\\d{1,2}/\\d{2,4}")) {
+//					cal.setTime(sdfNums.parse(bubbleProperties[2] + " "
+//							+ bubbleProperties[3]));
+//				} else if (bubbleProperties[2].trim().matches(
+//						"\\w{3,4}\\s+\\d{1,2},\\s+\\d{2,4}")) {
+//					cal.setTime(sdfLetters.parse(bubbleProperties[2] + " "
+//							+ bubbleProperties[3]));
+//				}
+//				TaskBubble t = new TaskBubble(new Task(bubbleProperties[0],
+//						cal, Long.parseLong(bubbleProperties[1])), 0, 100, 0);
+//				parent.addBubble(t);
+//				parent.setBubbleScreen();
+//			} catch (ParseException e) {
+//				e.printStackTrace();
+//			}
+//
+//			return;
 		}
-		bubbleProperties[i++] = text;
-		if (i >= 4) {
-			Calendar cal = Calendar.getInstance();
-			System.err.println("making task");
-			try {
-				if (bubbleProperties[2].trim().matches("\\d{1,2}/\\d{1,2}/\\d{2,4}")) {
-					cal.setTime(sdfNums.parse(bubbleProperties[2] + " "
-							+ bubbleProperties[3]));	
-				} else if(bubbleProperties[2].trim().matches("\\w{3,4}\\s+\\d{1,2},\\s+\\d{2,4}")){
-					cal.setTime(sdfLetters.parse(bubbleProperties[2] + " "
-							+ bubbleProperties[3]));
-				}
-				TaskBubble t = new TaskBubble(new Task(bubbleProperties[0],
-						cal, Long.parseLong(bubbleProperties[1])), 0, 100, 0);
-				parent.addBubble(t);
-				parent.setBubbleScreen();
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
 
-			return;
-		}
-
-		Gdx.input.getTextInput(new TextInput(TaskCreator.this), "New Task",
-				prompts[i]);
 	}
 
 	@Override
---
+++ desktop/src/com/bubbledone/main/desktop/NativeGetDate.java
@@ -0,0 +1,22 @@
+package com.bubbledone.platformspecific;
+
+import java.util.Calendar;
+
+import com.bubbledone.interfaces.GetDate;
+
+public class NativeGetDate implements GetDate {
+
+	@Override
+	public void getDate(TaskCreator parent) {
+
+	}
+
+	public void getEstimatedDuration(TaskCreator parent) {
+
+	}
+
+	public void getDueTime(TaskCreator parent) {
+
+	}
+
+}
