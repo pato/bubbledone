6f9301679be6ef896a3870a7001dd9feb779860f
Pato <plankenau@gmail.com>
2014-10-19 09:33:21 -0500

    changed delete button to cancel button

--- core/src/com/bubbledone/ui/BubbleInfoPopupRenderer.java
+++ core/src/com/bubbledone/ui/BubbleInfoPopupRenderer.java
@@ -21,15 +21,23 @@ public class BubbleInfoPopupRenderer {
         shapeRenderer.rect(11, 11, world.getWidth() - 20, world.getHeight() - 20);
         
         // box
-        shapeRenderer.setColor(83 / 255.0f, 109 / 255.0f, 254 / 255.0f, 1); //#536DFE
+        shapeRenderer.setColor(83 / 255.0f, 109 / 255.0f, 254 / 255.0f, 1); //#536DFE 
         shapeRenderer.rect(10, 10, world.getWidth() - 20, world.getHeight() - 20);
         
-        // delete button shadow
-        shapeRenderer.setColor(221 / 255.0f, 44 / 255.0f, 0 / 255.0f, 1); //#dd2c00
+//        // delete button shadow
+//        shapeRenderer.setColor(221 / 255.0f, 44 / 255.0f, 0 / 255.0f, 1); //#dd2c00
+//        shapeRenderer.rect(deleteBtn.x + 1, deleteBtn.y + 1, deleteBtn.width, deleteBtn.height);
+//        
+//        // delete button
+//        shapeRenderer.setColor(255 / 255.0f, 61 / 255.0f, 0 / 255.0f, 1); //#ff3d00
+//        shapeRenderer.rect(deleteBtn.x, deleteBtn.y, deleteBtn.width, deleteBtn.height);
+        
+        // cancel button shadow
+        shapeRenderer.setColor(158 / 255.0f, 158 / 255.0f, 158 / 255.0f, 1); //#9e9e9e
         shapeRenderer.rect(deleteBtn.x + 1, deleteBtn.y + 1, deleteBtn.width, deleteBtn.height);
         
-        // delete button
-        shapeRenderer.setColor(255 / 255.0f, 61 / 255.0f, 0 / 255.0f, 1); //#ff3d00
+        // cancel button
+        shapeRenderer.setColor(189 / 255.0f, 189 / 255.0f, 189 / 255.0f, 1); //#bdbdbd
         shapeRenderer.rect(deleteBtn.x, deleteBtn.y, deleteBtn.width, deleteBtn.height);
         
         // done button shadow
@@ -39,11 +47,10 @@ public class BubbleInfoPopupRenderer {
         // done button
         shapeRenderer.setColor(20 / 255.0f, 231 / 255.0f, 21 / 255.0f, 1); //#14e715
         shapeRenderer.rect(doneBtn.x, doneBtn.y, doneBtn.width, doneBtn.height);
-        
         shapeRenderer.end();
         
         world.getBatch().begin();
-        String text = "Delete";
+        String text = "Cancel";
         world.getFont().draw(world.getBatch(), text, deleteBtn.x - world.getFont().getBounds(text).width / 2 + doneBtn.width / 2,
         		deleteBtn.y - world.getFont().getBounds(text).height / 2 + deleteBtn.height / 2);
         
