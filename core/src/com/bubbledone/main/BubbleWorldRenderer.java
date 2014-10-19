ba4a917e7180f7e07f8f598bf93fa86c90babe2d
Rohan <rohan@cs.utexas.edu>
2014-10-18 23:25:16 -0500

    added background, kind of

--- core/src/com/bubbledone/main/BubbleWorldRenderer.java
+++ core/src/com/bubbledone/main/BubbleWorldRenderer.java
@@ -1,15 +1,12 @@
 package com.bubbledone.main;
 
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-
-import java.util.List;
 
 public class BubbleWorldRenderer {
 	private BubbleWorld world;
@@ -21,6 +18,7 @@ public class BubbleWorldRenderer {
 
 		cam = new OrthographicCamera();
 		cam.setToOrtho(true, 136, 204);
+				
 		shapeRenderer = new ShapeRenderer();
 		shapeRenderer.setProjectionMatrix(cam.combined);
 		
@@ -32,6 +30,10 @@ public class BubbleWorldRenderer {
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		world.getBatch().begin();
+        world.getBatch().draw(new Texture(Gdx.files.internal("background.jpg")),
+        		0, 0, 800, 480);
+        world.getBatch().end();
 		
 		// draw attractor
 		AttractorRenderer.render(world, world.getAttractor(), cam, shapeRenderer);
@@ -41,5 +43,7 @@ public class BubbleWorldRenderer {
         for (TaskBubble b : bubbles) {
         	TaskBubbleRenderer.render(world, b, cam, shapeRenderer);
         }
+        
+        
     }
 }
