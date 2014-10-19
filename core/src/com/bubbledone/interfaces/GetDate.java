18de1acfffbc6219b05effc905811345c5bf8041
Pato <plankenau@gmail.com>
2014-10-19 02:26:06 -0500

    Added passing of TaskCreator for callback

--- core/src/com/bubbledone/interfaces/GetDate.java
+++ core/src/com/bubbledone/interfaces/GetDate.java
@@ -1,7 +1,9 @@
 package com.bubbledone.interfaces;
 import java.util.Calendar;
 
+import com.bubbledone.main.TaskCreator;
+
 public interface GetDate {
-	public Calendar getDate();
+	public Calendar getDate(TaskCreator taskCreator);
 	public long getTime();
 }
