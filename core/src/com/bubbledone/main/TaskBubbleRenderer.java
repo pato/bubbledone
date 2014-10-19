a21382bec168cea3319a5160bc9732fc573f850b
Pato <plankenau@gmail.com>
2014-10-18 21:12:17 -0500

    fixed taskbubblerenderer to not use circle

--- core/src/com/bubbledone/main/TaskBubbleRenderer.java
+++ core/src/com/bubbledone/main/TaskBubbleRenderer.java
@@ -8,8 +8,8 @@ public final class TaskBubbleRenderer{
 	public static void render(BubbleWorld world, TaskBubble bubble, OrthographicCamera cam, ShapeRenderer shapeRenderer) {
         shapeRenderer.begin(ShapeType.Filled);
         shapeRenderer.setColor(83 / 255.0f, 109 / 255.0f, 254 / 255.0f, 1);
-        shapeRenderer.ellipse(bubble.getCircle().x, bubble.getCircle().y,
-        		bubble.getCircle().radius, bubble.getCircle().radius, 1000);
+        shapeRenderer.ellipse(bubble.getX(), bubble.getY(),
+        		bubble.getRadius(), bubble.getRadius(), 1000);
         shapeRenderer.end();
 	}
 }
