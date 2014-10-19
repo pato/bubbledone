37ba5af14f05a907154426660060eecd0e8a7e7d
Pato <plankenau@gmail.com>
2014-10-19 02:17:27 -0500

    Added platform specific get date methods

---
+++ core/src/com/bubbledone/platformspecific/AndroidGetDate.java
@@ -0,0 +1,15 @@
+package com.bubbledone.platformspecific;
+
+import java.util.Calendar;
+
+import com.bubbledone.interfaces.GetDate;
+
+public class AndroidGetDate implements GetDate{
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
