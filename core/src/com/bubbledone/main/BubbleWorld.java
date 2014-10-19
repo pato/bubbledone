b0756c428ce4b7978c7ecc44d413fcf784c554ce
William Yager <will.yager@gmail.com>
2014-10-18 21:21:43 -0500

    working physics

--- core/src/com/bubbledone/main/BubbleWorld.java
+++ core/src/com/bubbledone/main/BubbleWorld.java
@@ -4,6 +4,9 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.ArrayList;
 
+import com.badlogic.gdx.math.Vector2;
+import com.bubbledone.interfaces.Circular;
+
 public class BubbleWorld {
 	Attractor attractor;
 	List<TaskBubble> bubbles;
@@ -22,20 +25,38 @@ public class BubbleWorld {
 		bubbles.add(new TaskBubble(t,100,150,10));
 	}
 	
+	private Vector2 collide(Circular a, Circular b){
+		Vector2 diff = a.getPosition().cpy().sub(b.getPosition());
+		float radius = diff.len();
+		float distance = radius - a.getRadius() - b.getRadius();
+		float force_mag = (float) Math.exp(-20.0*distance);
+		return diff.scl(force_mag/radius);
+	}
+	
 	public void update(float delta) {
-		List<TaskBubble> new_bubbles = new ArrayList<TaskBubble>();
+		System.out.println(delta);
+		List<Vector2> new_velocities = new ArrayList<Vector2>();
+		List<Vector2> new_positions = new ArrayList<Vector2>();
 		for(int affectee_i=0; affectee_i<bubbles.size(); affectee_i++){
 			TaskBubble affectee = bubbles.get(affectee_i);
+			Vector2 total_force = new Vector2(0,0);
 			for(int affector_i=0; affector_i<bubbles.size(); affector_i++){
 				if(affector_i == affectee_i) continue; // Things don't affect themselves
 				TaskBubble affector = bubbles.get(affector_i);
-				double dx = affectee.getPosition().x - affector.getPosition().x;
-				double dy = affectee.getPosition().y - affector.getPosition().y;
-				double radius = Math.sqrt(dx*dx + dy*dy);
-				double distance = radius - affector.getRadius() - affectee.getRadius();
-				double force_mag = Math.exp(-20.0*distance);
-				double force_x = (dx/radius) * 
+				total_force = total_force.cpy().add(collide(affectee, affector));
 			}
+			total_force = total_force.cpy().add(collide(affectee, attractor));
+			double mass = Math.pow(affectee.getRadius(), 3);
+			double attractor_mass = Math.pow(attractor.getRadius(), 3);
+			Vector2 difference = affectee.getPosition().cpy().sub(attractor.getPosition());
+			double force_mag = -1 * mass * attractor_mass / Math.pow(difference.len(), 2);
+			Vector2 gravity_force = difference.cpy().scl((float) (force_mag/difference.len()));
+			total_force = total_force.cpy().add(gravity_force);
+			Vector2 acceleration = total_force.cpy().scl((float) (1/mass));
+			Vector2 velocity = affectee.getVelocity().cpy().add(acceleration.scl(delta));
+			Vector2 position = affectee.getPosition().cpy().add(velocity.scl(delta));
+			new_velocities.add(velocity);
+			new_positions.add(position);
 		}
     }
 	
